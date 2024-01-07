7a67e4cc2ac3bcbddc856dc5f8504fb472dfcc5d
polycastes <handog.adobo@gmail.com>
2024-01-07 10:15:43 +0800

    put service worker from controller to index

--- js/controller.js
+++ js/controller.js
@@ -1,36 +1,36 @@
-// Register the service worker
-if ('serviceWorker' in navigator) {
-  // Wait for the 'load' event to not block other work
-  window.addEventListener('load', async () => {
-    // Try to register the service worker.
-    try {
-      // Capture the registration for later use, if needed
-      let reg;
+// // Register the service worker
+// if ('serviceWorker' in navigator) {
+//   // Wait for the 'load' event to not block other work
+//   window.addEventListener('load', async () => {
+//     // Try to register the service worker.
+//     try {
+//       // Capture the registration for later use, if needed
+//       let reg;
 
-      // Use ES Module version of our Service Worker in development
-      if (import.meta.env?.DEV) {
-        reg = await navigator.serviceWorker.register(
-          '/skinclinic/service-worker.js',
-          {
-            type: 'module',
-            scope: '/skinclinic/',
-          }
-        );
-      } else {
-        // In production, use the normal service worker registration
-        reg = await navigator.serviceWorker.register(
-          '/skinclinic/service-worker.js',
-          {
-            scope: '/skinclinic/',
-          }
-        );
-      }
+//       // Use ES Module version of our Service Worker in development
+//       if (import.meta.env?.DEV) {
+//         reg = await navigator.serviceWorker.register(
+//           '/skinclinic/service-worker.js',
+//           {
+//             type: 'module',
+//             scope: '/skinclinic/',
+//           }
+//         );
+//       } else {
+//         // In production, use the normal service worker registration
+//         reg = await navigator.serviceWorker.register(
+//           '/skinclinic/service-worker.js',
+//           {
+//             scope: '/skinclinic/',
+//           }
+//         );
+//       }
 
-      console.log('Service worker registered! 😎', reg);
-    } catch (err) {
-      console.log('😥 Service worker registration failed: ', err);
-    }
-  });
-} else {
-  console.log('service worker not supported');
-}
+//       console.log('Service worker registered! 😎', reg);
+//     } catch (err) {
+//       console.log('😥 Service worker registration failed: ', err);
+//     }
+//   });
+// } else {
+//   console.log('service worker not supported');
+// }
